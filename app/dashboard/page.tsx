cc46255c2874b2aa1ddc7fefeed8bc802e61620e
Phoebe Gangoso <phoeberhonegangoso@gmail.com>
2025-10-26 02:34:38 +0800

    Refactor dashboard layout and styles

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,9 +4,15 @@ export default function Dashboard() {
   return (
     <div className="dashboard-container">
       <h2 className="dashboard-title">Dashboard</h2>
-      <p className="dashboard-description">
-        Welcome to your Solar-Powered Charging Station dashboard! Here you can see energy stats, available charging ports, and Wi-Fi usage.
-      </p>
+      <div className="wifi-container ">
+        <div className="wifi-time">59:45</div>
+        <div className="wifi-text">
+            <span className="wifi-bold">Wi-Fi</span>
+            <br />
+            <span className="wifi-subtext">Remaining Time</span>
+        </div>
+      </div>
+      <div className="line-separator"></div>
     </div>
   );
 }
