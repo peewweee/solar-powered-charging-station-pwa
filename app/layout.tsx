8c0295abcc0e70f635a3c00c41fe3a7972673800
peewweee <phoeberhonegangoso@gmail.com>
2026-04-25 17:55:48 +0800

    feat: update PWA configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,10 +24,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {/* PWA Theme */}
         <meta name="theme-color" content="#1E120B" />
         <meta name="background-color" content="#1E120B" />
+        <meta name="color-scheme" content="dark" />
 
         {/* PWA iOS Support */}
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="SPCS" />
 
         {/* Description */}
         <meta
