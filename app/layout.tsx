83117d44db71c1db8a8b599c05abc7c71d040d9b
peewweee <phoeberhonegangoso@gmail.com>
2026-04-25 19:15:30 +0800

    fix: restore apple-mobile-web-app-status-bar-style meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
         {/* PWA iOS Support */}
         <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="SPCS" />
 
         {/* Description */}
