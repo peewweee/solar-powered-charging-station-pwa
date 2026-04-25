d15170fe24a860e38ed7e63d6f8f30864d863544
peewweee <phoeberhonegangoso@gmail.com>
2026-04-25 18:54:22 +0800

    refactor: simplify min-height in globals.css

--- app/components/InstallationIdentityBootstrap.tsx
+++ app/components/InstallationIdentityBootstrap.tsx
@@ -6,27 +6,6 @@ import { ensureInstallationId } from "../lib/installation-id";
 export default function InstallationIdentityBootstrap() {
   useEffect(() => {
     ensureInstallationId();
-
-    const path = window.location.pathname;
-    const shouldNudgeScroll = path === "/" || path === "/dashboard";
-
-    if (!shouldNudgeScroll || window.scrollY > 0) {
-      return;
-    }
-
-    const nudgeScroll = () => {
-      if (window.scrollY === 0) {
-        window.scrollTo(0, 1);
-      }
-    };
-
-    const animationFrame = window.requestAnimationFrame(nudgeScroll);
-    const timeout = window.setTimeout(nudgeScroll, 150);
-
-    return () => {
-      window.cancelAnimationFrame(animationFrame);
-      window.clearTimeout(timeout);
-    };
   }, []);
 
   return null;
